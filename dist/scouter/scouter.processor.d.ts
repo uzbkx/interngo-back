0f085290741a752811fb953a7ea32319729756d9
omonovdev <omonovaxrorcc@gmail.com>
2026-03-28 00:54:14 +0500

    fix: remove scouter.processor (BullMQ), clean rebuild

--- dist/scouter/scouter.processor.d.ts
+++
@@ -1,8 +0,0 @@
-import { WorkerHost } from '@nestjs/bullmq';
-import { Job } from 'bullmq';
-import { ScouterService } from './scouter.service';
-export declare class ScouterProcessor extends WorkerHost {
-    private scouterService;
-    constructor(scouterService: ScouterService);
-    process(job: Job): Promise<any>;
-}
